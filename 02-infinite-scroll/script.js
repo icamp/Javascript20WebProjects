a9c0f43eb7246a6d45bf08f0a8d68ce6ceba9e2d
icamp <campan.ionel@gmail.com>
2021-10-11 09:32:18 +0300

    getting DRY

--- 02-infinite-scroll/script.js
+++ 02-infinite-scroll/script.js
@@ -4,19 +4,35 @@ const loader = document.getElementById('loader');
 
 let photosArray = [];
 
+// Helper function to set attributes on DOM elements
+function setAttributes (element, attributes) {
+   for (const key in attributes) {
+      element.setAttribute(key, attributes[key]);
+   }
+}
+
 // Create elements for links & photos, & add to DOM
 function displayPhotos() {
    // Run function for each object in the photosArray
    photosArray.forEach((photo) => {
       // Create <a> to link to Unsplash
       const item = document.createElement('a');
-      item.setAttribute('href', photo.links.html);
-      item.setAttribute('target', '_blank');
+      // item.setAttribute('href', photo.links.html);
+      // item.setAttribute('target', '_blank');
+      setAttributes(item, {
+         href: photo.links.html,
+         target: '_blank',
+      });
       // Create <img> for photo
       const img = document.createElement('img');
       img.setAttribute('src', photo.urls.regular);
       img.setAttribute('alt', photo.alt_description);
       img.setAttribute('title', photo.alt_description);
+      setAttributes(img, {
+         src: photo.urls.regular,
+         alt: photo.alt_description,
+         title: photo.alt_description,
+      });
       // Put <img> inside <a>, then put both inside imageContainer
       item.appendChild(img);
       imageContainer.appendChild(item);
